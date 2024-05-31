8d62ecae6e62e91d81bfa8951749b5c14e20082c
Abbos <kadirovabbos407@gmail.com>
2024-05-31 10:45:40 +0500

    exem7

--- src/lib/slice/wishlistSlice.js
+++ src/lib/slice/wishlistSlice.js
@@ -1,24 +1,24 @@
+"use client";
 import { createSlice } from "@reduxjs/toolkit";
+
 export const wishlistSlice = createSlice({
   name: "wishlist",
   initialState: {
-    value: [],
+    value: JSON.parse(localStorage.getItem("wishes")) || [],
   },
   reducers: {
-    setWishes(state, action) {
-      state.value = action.payload;
-    },
-    toggleLike(state, action) {
+    toogleLike(state, action) {
       let index = state.value.findIndex((el) => el.id === action.payload.id);
       if (index < 0) {
         state.value = [...state.value, action.payload];
+        localStorage.setItem("wishes", JSON.stringify(state.value));
       } else {
         state.value = state.value.filter((el) => el.id !== action.payload.id);
+        localStorage.setItem("wishes", JSON.stringify(state.value));
       }
-      localStorage.setItem("wishes", JSON.stringify(state.value));
     },
   },
 });
 
-export const { setWishes, toggleLike } = wishlistSlice.actions;
+export const { toogleLike } = wishlistSlice.actions;
 export default wishlistSlice.reducer;
